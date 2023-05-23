a1bc0639825b215b18a5e33640e363271999b8e0
VIVEK SITANIA <74618900+VivekSitania@users.noreply.github.com>
2023-05-24 00:20:12 +0530

    Time: 45 ms (16.07%), Space: 19.8 MB (86.52%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,22 @@
+class KthLargest {
+    int k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int num: nums){
+            pq.push(num);
+        }
+        while(pq.size() > k){
+            pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > this->k){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
