a715551004ad84230cf88c41be3fa44aa1b0d5b2
VIVEK SITANIA <74618900+VivekSitania@users.noreply.github.com>
2023-06-22 03:06:21 +0530

    Time: 444 ms (65.86%), Space: 0B (100.00%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,7 @@
+select 
+case when count(t1.salary) < 2 then NULL 
+else min(t1.salary) end as SecondHighestSalary
+from (select distinct salary
+from employee
+order by salary desc 
+limit 2) as t1
