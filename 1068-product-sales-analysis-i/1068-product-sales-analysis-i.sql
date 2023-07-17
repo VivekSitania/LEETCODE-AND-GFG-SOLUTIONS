f9e1a80bb32c1072182c0401815c20aa878a6404
VIVEK SITANIA <74618900+VivekSitania@users.noreply.github.com>
2023-07-18 03:39:27 +0530

    Time: 2175 ms (43.64%), Space: 0B (100.00%) - LeetHub

---
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT DISTINCT 
+    P.product_name, S.year, S.price 
+FROM 
+    (SELECT DISTINCT product_id, year, price FROM Sales) S
+INNER JOIN
+    Product AS P
+USING (product_id);
